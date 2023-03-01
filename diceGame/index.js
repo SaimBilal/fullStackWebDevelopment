8c55bd5ae2c987143ab1a451abc319c2bdd43f89
unknown <bilalsaimhd7870@gmail.com>
2023-03-02 00:56:57 +0100

    Dice Game Created.

---
+++ diceGame/index.js
@@ -0,0 +1,15 @@
+if (p1Score === p2Score) {
+    document.querySelector(".splash-heading").textContent = "IT'S A DRAW!";
+} else if (p1Score > p2Score) {
+    document.querySelector(".splash-heading").textContent = "PLAYER 1 WINS!";
+} else {
+    document.querySelector(".splash-heading").textContent = "PLAYER 2 WINS!";
+}
+
+var p1Score = Math.floor(Math.random() * 6);
+var p2Score = Math.floor(Math.random() * 6);
+
+var diceImgPaths = ["./images/dice1.png" , "./images/dice2.png", "./images/dice3.png", "./images/dice4.png", "./images/dice5.png", "./images/dice6.png"];
+
+document.querySelector(".dice-image1").setAttribute("src" , diceImgPaths[p1Score]);
+document.querySelector(".dice-image2").setAttribute("src" , diceImgPaths[p2Score]);
