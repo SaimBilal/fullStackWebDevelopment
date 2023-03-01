a6fd1608c6267c323403478ba871d614386fffdc
unknown <bilalsaimhd7870@gmail.com>
2023-03-02 00:58:26 +0100

    bugs sorted in index.js

--- diceGame/index.js
+++ diceGame/index.js
@@ -1,3 +1,8 @@
+var p1Score = Math.floor(Math.random() * 6);
+var p2Score = Math.floor(Math.random() * 6);
+
+var diceImgPaths = ["./images/dice1.png" , "./images/dice2.png", "./images/dice3.png", "./images/dice4.png", "./images/dice5.png", "./images/dice6.png"];
+
 if (p1Score === p2Score) {
     document.querySelector(".splash-heading").textContent = "IT'S A DRAW!";
 } else if (p1Score > p2Score) {
@@ -5,11 +10,6 @@ if (p1Score === p2Score) {
 } else {
     document.querySelector(".splash-heading").textContent = "PLAYER 2 WINS!";
 }
-
-var p1Score = Math.floor(Math.random() * 6);
-var p2Score = Math.floor(Math.random() * 6);
-
-var diceImgPaths = ["./images/dice1.png" , "./images/dice2.png", "./images/dice3.png", "./images/dice4.png", "./images/dice5.png", "./images/dice6.png"];
 
 document.querySelector(".dice-image1").setAttribute("src" , diceImgPaths[p1Score]);
 document.querySelector(".dice-image2").setAttribute("src" , diceImgPaths[p2Score]);
