d1cf21cc7d8eac7c2cbef179cc2852084de634d3
unknown <bilalsaimhd7870@gmail.com>
2023-03-02 01:12:15 +0100

    Further code optimization.

--- diceGame/index.js
+++ diceGame/index.js
@@ -1,7 +1,10 @@
-var p1Score = Math.floor(Math.random() * 6);
-var p2Score = Math.floor(Math.random() * 6);
+var p1Score = Math.floor(Math.random() * 6) + 1;
+var p2Score = Math.floor(Math.random() * 6) + 1;
 
-var diceImgPaths = ["./images/dice1.png" , "./images/dice2.png", "./images/dice3.png", "./images/dice4.png", "./images/dice5.png", "./images/dice6.png"];
+// var p1Score = Math.floor(Math.random() * 6);
+// var p2Score = Math.floor(Math.random() * 6);
+
+// var diceImgPaths = ["./images/dice1.png" , "./images/dice2.png", "./images/dice3.png", "./images/dice4.png", "./images/dice5.png", "./images/dice6.png"];
 
 if (p1Score === p2Score) {
     document.querySelector(".splash-heading").textContent = "IT'S A DRAW!";
@@ -11,5 +14,8 @@ if (p1Score === p2Score) {
     document.querySelector(".splash-heading").textContent = "PLAYER 2 WINS!";
 }
 
-document.querySelector(".dice-image1").setAttribute("src" , diceImgPaths[p1Score]);
-document.querySelector(".dice-image2").setAttribute("src" , diceImgPaths[p2Score]);
+document.querySelector(".dice-image1").setAttribute("src" , "./images/dice" + p1Score + ".png");
+document.querySelector(".dice-image2").setAttribute("src" , "./images/dice" + p2Score + ".png");
+
+// document.querySelector(".dice-image1").setAttribute("src" , diceImgPaths[p1Score]);
+// document.querySelector(".dice-image2").setAttribute("src" , diceImgPaths[p2Score]);
